a1354531df7ff35e06d6dffa30ed80cdf6ba8876
PedroH <pedro2266809ph>
2023-09-07 09:35:40 -0300

    feat add formatting mask

--- src/modules/GeraCPF.js
+++ src/modules/GeraCPF.js
@@ -5,11 +5,20 @@ export default class GeraCPF {
         return  String(Math.floor(Math.random() * (max - min) + min));
     }
 
+    formatar(cpf) {
+        return(
+            cpf.slice(0, 3) + '.' +
+            cpf.slice(3, 6) + '.' +
+            cpf.slice(6, 9) + '-' +
+            cpf.slice(9, 11)
+        )
+    } 
+
     geraNewCpf() {
         const cpfSemDigito = this.rand();
         const digito1  = ValidaCpf.criaDigit(cpfSemDigito);
         const digito2  = ValidaCpf.criaDigit(cpfSemDigito + digito1);
         const newCpf = cpfSemDigito + digito1 + digito2;
-        return newCpf;
+        return this.formatar(newCpf);
     }
 }
